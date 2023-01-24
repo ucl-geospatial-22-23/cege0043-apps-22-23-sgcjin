0328dc17a996c0368ebc39b43a58bb486463f648
sgcjin <sgcjin@gmail.com>
2023-01-24 08:14:12 +0000

    add busstop.js with initial structures of the function to add and remove busstop data

---
+++ js/busstops.js
@@ -0,0 +1,8 @@
+"use strict";
+var busstopslayer;
+function getBusStopsData() {
+alert("loading busstopslayer");
+}
+function removeBusStopsData() {
+    alert("Earthquake data will be removed");
+}
