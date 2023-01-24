7220554c44464c32c4eb19ff4e337de8a7731520
sgcjin <sgcjin@gmail.com>
2023-01-24 10:59:47 +0000

    load ethernet geojson link

--- js/ethernetcables.js
+++ js/ethernetcables.js
@@ -1,7 +1,12 @@
 "use strict";
 let ethernetcableslayer;
 function getEthernetData(){
-alert("Ethernet cables data will be loaded");
+
+console.log(document.location.origin + "/data/ethernet.geojson");
+let busstopURL = document.location.origin + "/data/ethernet.geojson";
+
+
+
 }
 function removeEthernetData(){
 alert("Ethernet cables data will be removed");
