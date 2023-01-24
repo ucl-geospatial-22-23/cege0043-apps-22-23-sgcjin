fde456914deb0edb929b0c7a55c024580ccc29b0
sgcjin <sgcjin@gmail.com>
2023-01-24 10:54:01 +0000

    add ethernetcables.js with its add and remove button

---
+++ js/ethernetcables.js
@@ -0,0 +1,8 @@
+"use strict";
+let ethernetcableslayer;
+function getEthernetData(){
+alert("Ethernet cables data will be loaded");
+}
+function removeEthernetData(){
+alert("Ethernet cables data will be removed");
+}
