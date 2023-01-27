25806daf64510ed4fa6668c574c9de62edc37209
sgcjin <sgcjin@gmail.com>
2023-01-27 15:31:56 +0000

    add menu.js with menu click functions

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+"use strict";
+function menu1(){
+alert("menu 1 clicked")
+}
+function menu2(){
+alert("menu 1 clicked")
+}
+function menu3(){
+alert("menu 1 clicked")
+}
+function menu4(){
+alert("menu 1 clicked")
+}
+function menu6(){
+alert("menu 1 clicked")
+}
+function menu7(){
+alert("menu 1 clicked")
+}
+function menu8(){
+alert("menu 1 clicked")
+}
+function menu9(){
+alert("menu 1 clicked")
+}
+function menu10(){
+alert("menu 1 clicked")
+}
+function search(){
+	alert("menu 1 clicked")
+}
