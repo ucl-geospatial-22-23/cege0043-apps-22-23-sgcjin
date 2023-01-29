68172af207f2647c8a31e9c0d4e2ffb0ca65a5e6
sgcjin <sgcjin@gmail.com>
2023-01-29 20:16:35 +0000

    remove functions in menu.js that dosen't have onclick

--- js/menu.js
+++ js/menu.js
@@ -2,28 +2,39 @@
 function menu1(){
 alert("menu 1 clicked")
 }
-function menu2(){
-alert("menu 2 clicked")
-}
+// menu 2
 function menu3(){
 alert("menu 1 clicked")
 }
 function menu4(){
 alert("menu 1 clicked")
 }
+// menu 5
 function menu6(){
 alert("menu 1 clicked")
 }
 function menu7(){
 alert("menu 1 clicked")
 }
-function menu8(){
-alert("menu 1 clicked")
-}
+// menu 8
 function menu9(){
 alert("menu 1 clicked")
 }
 function menu10(){
+alert("menu 1 clicked")
+}
+// menu 11
+function menu12(){
+alert("menu 1 clicked")
+}
+function menu13(){
+alert("menu 1 clicked")
+}
+// menu 12
+function menu15(){
+alert("menu 1 clicked")
+}
+function menu16(){
 alert("menu 1 clicked")
 }
 function search(){
