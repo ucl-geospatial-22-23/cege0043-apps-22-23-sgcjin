35360cebe5f01ec0037e62c4e7deaca096270c6c
sgcjin <sgcjin@gmail.com>
2023-03-16 03:22:41 +0000

    add compare condition function and AJAX request in conditionSurvey.js

---
+++ js/conditionSurvey.js
@@ -0,0 +1,61 @@
+"use strict";
+	if (document.getElementById("morning").checked) {
+ 		 postString=postString+"&lecturetime=morning";
+	}
+	if (document.getElementById("afternoon").checked) {
+ 		 postString=postString+"&lecturetime=afternoon";
+	}
+
+function saveCondition(){
+    let postString = "";
+
+	// The previous condition (from the hidden field)
+	let previousConditionValue = document.getElementById("previousConditionValue").value;
+	// The ID of the asset (from the hidden field)
+	let assetID = document.getElementById("assetID").value;
+	let condition = "";
+	
+	if (document.getElementById("con_1").checked) {
+ 		 condition = "1";
+	}
+	if (document.getElementById("con_2").checked) {
+ 		 condition = "2";
+	}
+	if (document.getElementById("con_3").checked) {
+ 		 condition = "3";
+	}
+	if (document.getElementById("con_4").checked) {
+ 		 condition = "4";
+	}
+	if (document.getElementById("con_5").checked) {
+ 		 condition = "5";
+	}
+
+	if (condition == previousConditionValue){
+		alert("The the selected condition is the same as the previous condition");
+		postString = postString + "&answer=True"
+	}else{
+		alert("The the selected condition is NOT the same as the previous condition");
+		postString = postString + "&answer=False"
+	}
+	processData(postString)    
+}
+
+function processData(postString) {
+	let serviceUrl= document.location.origin +"/api/testCRUD"
+	//serviceUrl = "https://cege0043-7.cs.ucl.ac.uk/api/testCRUD"
+	$.ajax({
+	url: serviceUrl,
+	crossDomain: true,
+	type: "POST",
+	success: function(data){console.log(data); dataUploaded(data);},
+	data: postString
+	});
+}
+
+// create the code to process the response from the data server
+function conditionResult(data) {
+	// change the DIV to show the response
+	document.getElementById("responseDIV").innerHTML =
+	JSON.stringify(data);
+	}
