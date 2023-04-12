e6fad3bf4d1e02590b43d940871c27de005f17b7
sgcjin <sgcjin@gmail.com>
2023-04-12 07:03:21 +0100

    change graph wrapper name

--- js/graph.js
+++ js/graph.js
@@ -6,7 +6,7 @@ function closeAssetData(){
  toggle: false, show:false
  });
  bsMapCollapse.show();
- let adwCollapse = document.getElementById('assetDataWrapperWrapper');
+ let adwCollapse = document.getElementById('graphDataWrapperWrapper');
  let bsAdwCollapse = new bootstrap.Collapse(adwCollapse, {
  toggle: false, show:true
  });
@@ -20,19 +20,19 @@ function loadGraph(){
  toggle: false, show:false
  });
  bsMapCollapse.hide();
- let adwCollapse = document.getElementById('assetDataWrapperWrapper');
+ let adwCollapse = document.getElementById('graphDataWrapperWrapper');
  let bsAdwCollapse = new bootstrap.Collapse(adwCollapse, {
  toggle: false, show:true
  });
  bsAdwCollapse.show();
  
 // code to create the graph goes here – see below
-let widtha = document.getElementById("assetDataWrapper").clientWidth*2;
-let heighta = document.getElementById("assetDataWrapper").offsetHeight;
+let widtha = document.getElementById("graphDataWrapper").clientWidth*2;
+let heighta = document.getElementById("graphDataWrapper").offsetHeight;
 
  
  // Add the close button and an SVG element for the graph
- document.getElementById("assetDataWrapper").innerHTML=`<div class="h-100 w-100">
+ document.getElementById("graphDataWrapper").innerHTML=`<div class="h-100 w-100">
  <button type="button" class="btn-close float-end" arialabel="Close" onclick="closeAssetData()"></button>
  <svg fill="blue" width="`+widtha+`" height="`+heighta+`" id="svg1">
  </svg>
