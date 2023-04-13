5a05e01dabb122d27271a02711230ef273921434
sgcjin <sgcjin@gmail.com>
2023-04-13 14:27:27 +0100

    add ajax request for user id and assets

--- js/loadDashboard.js
+++ js/loadDashboard.js
@@ -1,3 +1,40 @@
-function setUpDashboard() {
+// store the asset features
+let Assetfeatures;
+// set base url
+let baseURL = "https://cege0043-7.cs.ucl.ac.uk";
+
+async function setUpDashboard() {
+    await getAssets();
+    console.log(Assetfeatures);
     
 }
+
+
+// get asset features
+function getAssets() {
+    return new Promise((resolve)=>{
+        // get user id first
+        $.ajax({
+            url: baseURL + "/api/userID",
+            crossDomain: true,
+            success: function(result) {
+                let userID = JSON.parse(JSON.stringify(result))[0].user_id;
+                // after get user id, get userAssets
+                $.ajax({
+                    url: baseURL + "/api/geojson/userAssets/" + userID,
+                    crossDomain: true,
+                    success: function(result) {
+                        // get features
+                        Assetfeatures = result.features;
+                        resolve(Assetfeatures);
+                    }
+                });
+                // end of AJAX userAssets
+            }
+        });
+        // end  of AJAX user ID
+    }
+    );
+
+}
+// end of function
